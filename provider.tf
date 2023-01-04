e7e5f7d0656f8cce79e99e78f1dbbfb5ec547fe2
91994 <suddala.rajukumar123@gmail.com>
2023-01-04 19:22:22 +0530

    updating

--- provider.tf
+++ provider.tf
@@ -1,4 +1,18 @@
-provider "aws"{
-  region = "us-east-1"
-  alias  = "east"
+#provider "aws"{
+#  region = "us-east-1"
+#  alias  = "east"
+#}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+
 }
