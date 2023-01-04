0673ffaeaf4a8f58b35966e99af8c430a1cad864
91994 <suddala.rajukumar123@gmail.com>
2023-01-04 18:20:24 +0530

    updating

--- provider.tf
+++ provider.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
-#provider "aws" {
-## region = "${var.region}"
-#  region = "us-east-1"
-##  profile = "prashant_appgambit="
-##  profile = "raju-tf-jk-it"
-##  profile = "prashant_appgambit"
-#}
