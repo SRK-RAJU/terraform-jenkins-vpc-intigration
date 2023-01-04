cffece9d3598b60956e0acfd9cff920a578693d2
91994 <suddala.rajukumar123@gmail.com>
2023-01-04 17:17:52 +0530

    updating

--- provider.tf
+++ provider.tf
@@ -1,7 +1,17 @@
-provider "aws" {
-# region = "${var.region}"
-  region = "us-east-1"
-#  profile = "prashant_appgambit="
-#  profile = "raju-tf-jk-it"
-  profile = "prashant_appgambit"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
 }
+provider "aws" {
+  region = "us-east-1"
+}
+#provider "aws" {
+## region = "${var.region}"
+#  region = "us-east-1"
+##  profile = "prashant_appgambit="
+##  profile = "raju-tf-jk-it"
+##  profile = "prashant_appgambit"
+#}
